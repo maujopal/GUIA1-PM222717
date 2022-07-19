326fb909a4c72ff8da5e612a1c0e5587ef8983f9
maujopal <109572169+maujopal@users.noreply.github.com>
2022-07-19 07:47:59 -0600

    initial commit

---
+++ Maquetación - Eje 3/JavaScript/maquetacion.js
@@ -0,0 +1,13 @@
+var Header = "Header";
+var Navigation = "Navigation";
+var Article = "Article";
+var Footer = "Footer";
+
+
+
+document.write("<header>" + Header + "</header>");
+document.write("<navigation>" + Navigation + "</navigation>");
+document.write("<article>" + Article + "</article>");
+document.write("<article>" + Article + "</article>");
+document.write("<article>" + Article + "</article>");
+document.write("<footer>" + Footer + "</footer>");
